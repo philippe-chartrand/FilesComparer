b4b991caa0eba5557c9e97de710b7be1cf9d4047
Philippe Chartrand <philippe.chartrand@videotron.ca>
2026-01-24 11:44:52 -0500

    récupération des nouveaux fichiers

--- test-setup.sh
+++ test-setup.sh
@@ -4,12 +4,14 @@ git checkout A/changed-in-destination.txt
 git checkout A/changed-in-source.txt
 git checkout A/new.txt
 git checkout A/same.txt
+git checkout A/same/fichier*txt
 
 git checkout B/to-be-moved/same.txt
 git checkout B/changed-in-destination.txt
 git checkout B/changed-in-source.txt
 git checkout B/old.txt
 git checkout B/same.txt
+git checkout B/same/fichier*txt
 
 touch A/changed-in-source.txt
 touch B/changed-in-destination.txt
