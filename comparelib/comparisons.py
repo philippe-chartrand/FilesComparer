b323adb2051665a10094e0a4af7b5ab3a6b097f8
Philippe Chartrand <philippe.chartrand@videotron.ca>
2026-03-30 11:12:27 -0400

    replace n**2 logic by nlog(n) by using md5 indexes directly

--- comparelib/comparisons.py
+++ comparelib/comparisons.py
@@ -1,3 +1,5 @@
+from os.path import basename
+
 
 def minus (dir_one, dir_two):
     remainder = { k:dir_one[k] for k in dir_one.keys() if k not in dir_two}
@@ -24,15 +26,36 @@ def modified(common):
             unchanged[k] = [first,second]
     return changed_in_dir_one, changed_in_dir_two, unchanged
 
+def index_by_checksum(items):
+    checksums = {}
+    for k, v in items.items():
+        if not v['md5'] in checksums:
+            checksums[v['md5']] = [k]
+        else:
+            checksums[v['md5']].append(k)
+    return checksums
 
 def find_moved(removed, added):
     moved = {}
-    for previous_file in removed.copy():
-        for new_file in added.copy():
-            if previous_file != new_file:
-                if previous_file in removed and new_file in added:
-                    if removed[previous_file]['md5'] == added[new_file]['md5']:
-                        moved[previous_file] = [removed[previous_file], added[new_file]]
-                        del removed[previous_file]
-                        del added[new_file]
+    added_checksums = index_by_checksum(added)
+    removed_checksums = index_by_checksum(removed)
+    common_checksums = { k for k in added_checksums.keys() if k in removed_checksums }
+
+    for common_md5 in common_checksums:
+        new_file_paths = added_checksums[common_md5]
+        for new_file_path in new_file_paths:
+            for previous_file_path in removed_checksums[common_md5]:
+                if basename(new_file_path) == basename(previous_file_path):
+                    src = removed.get(previous_file_path)
+                    if src is None:
+                        print("find_moved: No such file in removed files: ", previous_file_path)
+                        continue
+                    dst = added.get(new_file_path)
+                    if dst is None:
+                        print("find_moved: No such file in added files: ", new_file_path)
+                        continue
+                    moved[previous_file_path] = (src, dst)
+                    del removed[previous_file_path]
+                    del added[new_file_path]
+
     return moved
