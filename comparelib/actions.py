072ec04315aaedbb2bfa7e25b082a0dceb25829f
Philippe Chartrand <philippe.chartrand@videotron.ca>
2026-04-02 11:57:49 -0400

    in batch mode, we need to distinguish execution (never) and printing (only when simulating).

--- comparelib/actions.py
+++ comparelib/actions.py
@@ -20,11 +20,11 @@ def cleanup_empty_dirs(path, execute_now):
                 pass
 
 
-def move(moved, dir_one_path, dir_two_path, dir_two, execute_now):
+def move(moved, dir_one_path, dir_two_path, dir_two, execute_now, print_action=True):
     actions = []
     for k in sorted(moved.keys()):
         try:
-            d1, d2, dest_dir, new_path, old_path = move_prepare_one(dir_one_path, dir_two_path, k, moved)
+            d1, d2, dest_dir, new_path, old_path = move_prepare_one(dir_one_path, dir_two_path, k, moved, print_action)
             if execute_now:
                 move_one(dest_dir, new_path, old_path)
                 update_dir_two(d2, dir_one_path, dir_two, k, new_path)
@@ -35,17 +35,18 @@ def move(moved, dir_one_path, dir_two_path, dir_two, execute_now):
             pass
     return actions
 
-def move_prepare_one(dir_one_path, dir_two_path, k, moved):
+def move_prepare_one(dir_one_path, dir_two_path, k, moved, print_action):
     d1 = moved[k][0]
     d2 = moved[k][1]
     old_path = str(d1['path'])
     new_path = dir_two_path + '/' + remove_prefix(d2['path'], dir_one_path)
     partial_dest = make_partial_dest(d2['path'], dir_one_path)
     dest_dir = dir_two_path + partial_dest
-    if old_path != new_path:
-        print(f"mv \"{old_path}\" \"{new_path}\"")
-    else:
-        print(f"moving {old_path} on {new_path} is useless.")
+    if print_action:
+        if old_path != new_path:
+            print(f"mv \"{old_path}\" \"{new_path}\"")
+        else:
+            print(f"moving {old_path} on {new_path} is useless.")
     return d1, d2, dest_dir, new_path, old_path
 
 
@@ -60,11 +61,11 @@ def update_dir_two(d2, dir_one_path, dir_two, k, new_path):
     del dir_two[k]
 
 
-def add(to_add, dir_one_path, dir_two_path, dir_two, execute_now):
+def add(to_add, dir_one_path, dir_two_path, dir_two, execute_now, print_action=True):
     actions = []
     for k in sorted(to_add):
         try:
-            dest_dir, d = add_prepare_one(dir_one_path, dir_two_path, k, to_add)
+            dest_dir, d = add_prepare_one(dir_one_path, dir_two_path, k, to_add, print_action)
             if execute_now:
                 add_one(d, dest_dir, dir_two, k)
             else:
@@ -85,11 +86,12 @@ def add_update_cache_one(d, dest_dir, dir_two, k):
     dir_two[k]['path'] = Path(f"{dest_dir}/{k}")
 
 
-def add_prepare_one(dir_one_path, dir_two_path, k, to_add):
+def add_prepare_one(dir_one_path, dir_two_path, k, to_add, print_action):
     d = to_add[k]
     partial_dest = make_partial_dest(d['path'], dir_one_path)
     dest_dir = dir_two_path + "/" + partial_dest
-    print(f"cp \"{d['path']}\" \"{dest_dir}\"")
+    if print_action:
+        print(f"cp \"{d['path']}\" \"{dest_dir}\"")
     return dest_dir, d
 
 
@@ -102,11 +104,11 @@ def compare_mtimes(mtime1, mtime2):
         return "<"
 
 
-def update(changed, dir_two, execute_now):
+def update(changed, dir_two, execute_now, print_action=True):
     actions = []
     for k in sorted(changed.keys()):
         try:
-            source, destination = update_prepare_one(changed, k)
+            source, destination = update_prepare_one(changed, k, print_action)
             if execute_now:
                 update_one(changed, destination, dir_two, k, source)
             else:
@@ -123,26 +125,27 @@ def update_one(changed, destination, dir_two, k, source):
     dir_two[k]['md5'] = changed[k][0]['md5']
 
 
-def update_prepare_one(changed, k):
+def update_prepare_one(changed, k, print_action):
     source = changed[k][0]['path']
     destination = changed[k][1]['path']
-    if changed[k][0]['mtime'] != changed[k][1]['mtime']:
-        comparator = compare_mtimes(changed[k][0]['mtime'], changed[k][1]['mtime'])
-        print(
-            f"# changed: {datetime.datetime.fromtimestamp(changed[k][0]['mtime'])} {comparator} {datetime.datetime.fromtimestamp(changed[k][1]['mtime'])}")
-    if changed[k][0]['size'] != changed[k][1]['size']:
-        print(f"# size: {changed[k][0]['size']} != {changed[k][1]['size']}")
-    if changed[k][0]['md5'] != changed[k][1]['md5']:
-        print(f"# md5: {changed[k][0]['md5']} != {changed[k][1]['md5']}")
-    print(f"cp \"{source}\" \"{destination}\"")
+    if print_action:
+        if changed[k][0]['mtime'] != changed[k][1]['mtime']:
+            comparator = compare_mtimes(changed[k][0]['mtime'], changed[k][1]['mtime'])
+            print(
+                f"# changed: {datetime.datetime.fromtimestamp(changed[k][0]['mtime'])} {comparator} {datetime.datetime.fromtimestamp(changed[k][1]['mtime'])}")
+        if changed[k][0]['size'] != changed[k][1]['size']:
+            print(f"# size: {changed[k][0]['size']} != {changed[k][1]['size']}")
+        if changed[k][0]['md5'] != changed[k][1]['md5']:
+            print(f"# md5: {changed[k][0]['md5']} != {changed[k][1]['md5']}")
+        print(f"cp \"{source}\" \"{destination}\"")
     return source, destination
 
 
-def restore(changed, dir_one, execute_now):
+def restore(changed, dir_one, execute_now, print_action=True):
     actions = []
     for k in sorted(changed.keys()):
         try:
-            source, destination = restore_prepare_one(changed, k)
+            source, destination = restore_prepare_one(changed, k, print_action)
             if execute_now:
                 restore_one(changed, destination, dir_one, k, source)
             else:
@@ -159,27 +162,29 @@ def restore_one(changed, destination, dir_one, k, source):
     dir_one[k]['md5'] = changed[k][1]['md5']
 
 
-def restore_prepare_one(changed, k):
+def restore_prepare_one(changed, k, print_action):
     source = changed[k][0]['path']
     destination = changed[k][1]['path']
-    if changed[k][0]['mtime'] != changed[k][1]['mtime']:
-        comparator = compare_mtimes(changed[k][0]['mtime'], changed[k][1]['mtime'])
-        print(
-            f"# changed: {datetime.datetime.fromtimestamp(changed[k][0]['mtime'])} {comparator} {datetime.datetime.fromtimestamp(changed[k][1]['mtime'])}")
-    if changed[k][0]['size'] != changed[k][1]['size']:
-        print(f"# size: {changed[k][0]['size']} != {changed[k][1]['size']}")
-    if changed[k][0]['md5'] != changed[k][1]['md5']:
-        print(f"# md5: {changed[k][0]['md5']} != {changed[k][1]['md5']}")
-    print(f"cp \"{destination}\" \"{source}\"")
+    if print_action:
+        if changed[k][0]['mtime'] != changed[k][1]['mtime']:
+            comparator = compare_mtimes(changed[k][0]['mtime'], changed[k][1]['mtime'])
+            print(
+                f"# changed: {datetime.datetime.fromtimestamp(changed[k][0]['mtime'])} {comparator} {datetime.datetime.fromtimestamp(changed[k][1]['mtime'])}")
+        if changed[k][0]['size'] != changed[k][1]['size']:
+            print(f"# size: {changed[k][0]['size']} != {changed[k][1]['size']}")
+        if changed[k][0]['md5'] != changed[k][1]['md5']:
+            print(f"# md5: {changed[k][0]['md5']} != {changed[k][1]['md5']}")
+        print(f"cp \"{destination}\" \"{source}\"")
     return source, destination
 
 
-def remove(deleted, dir_two, execute_now):
+def remove(deleted, dir_two, execute_now, print_action=True):
     actions = []
     for k in sorted(deleted.keys()):
         d=deleted[k]
         try:
-            remove_prepare_one(d)
+            if print_action:
+                remove_print_one(d)
             if execute_now:
                 remove_one(d, dir_two, k)
             else:
@@ -195,5 +200,5 @@ def remove_one(d, dir_two, k):
     del dir_two[k]
 
 
-def remove_prepare_one(d):
+def remove_print_one(d):
     print(f"rm \"{d['path']}\"")
